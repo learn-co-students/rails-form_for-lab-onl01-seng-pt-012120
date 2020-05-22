7ef579f05d3c48cea84e470229409c093c559438
d-otis <dan@dan-foley.com>
2020-05-21 20:46:41 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Dan", last_name: "Foley")
+Student.create(first_name: "Danny", last_name: "Orendorff")
+Student.create(first_name: "Jesse", last_name: "Harrod")
+Student.create(first_name: "Leigh", last_name: "Dotey")
+
+SchoolClass.create(title: "Math 101", room_number: 1)
+SchoolClass.create(title: "Home Ec", room_number: 3)
+SchoolClass.create(title: "Advanced JS", room_number: 7)
+SchoolClass.create(title: "TensorFlow", room_number: 2)
